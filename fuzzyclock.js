9ae1029770342d54ee01429ae1c6c84c64caaf06
Horia Dragomir <horia@hdragomir.com>
2011-10-30 22:45:43 +0100

    moved new fuzzycode to the old js file

--- fuzzyclock.js
+++ fuzzyclock.js
@@ -1,189 +1,120 @@
 /**
  * @author Horia Dragomir horia@hdragomir.com http://hdragomir.com
  */
-var fuzzyclock = function( element, options ){
-    var fz = {
-        /** holds the actual element */
-        canvas : null,
-        /** holds the current date object */
-        date : null,
-        /** holds the compiled time string */
-        timeString : '',
-        /** holds the interval id */
-        interval : 0,
-        /** public options */
-        options : {
-            "interval" : 180000,
-            'silent' : true
-        },
-        /** mapps numbers to words for hours */
-        hoursAsStringArray : [
-            'twelve',
-            'one',
-            'two',
-            'three',
-            'four',
-            'five',
-            'six',
-            'seven',
-            'eight',
-            'nine',
-            'ten',
-            'eleven',
-            'twelve'
-        ],
-        /** maps numbers to words for minutes
-         * or actually their ranges
-         */
-        minutesAsStringArray : {
-            "5"     : 'five',
-            "10"    : 'ten',
-            "15"    : 'quarter',
-            "20"    : 'twenty',
-            "30"    : 'half'
-        },
-        /** maps what comes in between the verbal representation of minutes
-         * and the verbal representation of hours
-         * ignored if time is sharp ( :56 -> :04 )
-         */
-        infixes : { "to" : 'to', "past" : 'past' },
-        /** appended to the time string when hour is sharp ( :56 -> :04 ) */
-        postfixes : { "sharp" : "o'clock" },
-        /**
-         * function update
-         * regenerates the internal timestring and then calls the draw function
-         * @returns {function} this
-         * @private
-         */
-        update : function(){
-            this.date = new Date();
-            var hours = this.date.getHours(),
-                minutes = this.date.getMinutes(),
-                MASA = this.minutesAsStringArray,
-                hoursAsString, minutesAsString, infix, postfix = '';
-            if ( minutes <= 33 ) {
-                infix = this.infixes.past;
-            } else {
-                infix = this.infixes.to;
-                minutes = 60 - minutes;
-                hours++;
-            }
-            hours >= 12 && ( hours -= 12 );
-            hoursAsString = this.hoursAsStringArray[ hours ];
-            if ( minutes < 4 ) {
-                minutesAsString = "";
-                postfix = this.postfixes.sharp;
-                infix = "";
-            } else if ( minutes < 8 ) {
-                minutesAsString = MASA['5'];
-            } else if ( minutes < 13 ) {
-                minutesAsString = MASA['10'];
-            } else if ( minutes < 18 ) {
-                minutesAsString = MASA['15'];
-            } else if ( minutes < 27 ) {
-                minutesAsString = MASA['20'];
-            } else {
-                minutesAsString = MASA['30'];
-            }
-            this.timeString = minutesAsString + " " + infix + " " + hoursAsString + " " + postfix;
-            return this.draw();
-        },
-        /**
-         * function draw
-         * this function writes the current time string to the canvas element
-         * @param   override    {stirng}    set the canvas to a custom string
-         * @returns {funciton}  this
-         * @private
-         */
-        draw : function( override ) {
-            var canvas = this.canvas.cloneNode( false );
-            canvas.appendChild( this.canvas.ownerDocument.createTextNode( override || this.timeString ) );
-            this.canvas.parentNode.replaceChild( canvas, this.canvas );
-            this.canvas = canvas;
-            return this;
-        },
-        /**
-         * function init
-         * saves the canvas and options
-         * @param   element     {HTMLElement}   the element that holds the clock
-         * @param   options     {hash}          the options
-         * current options are:
-         *      interval    {number}    the update frequency
-         *      silent      {boolean}   whether to throw exceptions or not
-         *
-         * @throws Exception
-         * @returns {function} this
-         */
-        init : function( element, options ){
-
-            this.options.interval = options.interval || this.options.interval;
-
-            for( var opt in options ){
-                this.options[opt] = options[opt];
-            }
-
-            this.canvas = element;
-
-            if( ! this.canvas && ! this.options.silent ){
-                throw 'fuzzyClock : Could not do anything with "' + element + '"';
-            }
-
-            return this.start();
-        },
-        /**
-         * function start
-         * sets the update interval, uses scope binding.
-         * calls the update function
-         * @returns {function} this ( via this.update() )
-         */
-        start : function(){
-            this.interval = window.setInterval( ( function( scope ){
-                return function(){
-                    scope.update.call( scope );
-                };
-            } )( this ), this.options.interval );
-            return this.update();
-        },
-        /**
-         * function stop
-         * clears the update interval
-         * @returns {function} this
-         */
-        stop : function(){
-            window.clearInterval( this.interval );
-            return this;
-        },
-        /**
-         * convenient method to get the time string
-         */
-        toString : function(){
-            return this.timeString;
-        }
+(function (window, document, undefined) {
+    
+    var translateMinutes = {
+        30: "half",
+        20: "twenty",
+        15: "quarter",
+        10: "ten",
+        5: "five",
+        0: ''
     },
-    /** preset the canvas to null */
-    el;
+        translateHours = "twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven".split(','),
+        infix = {
+            past: "past",
+            to: "to"
+        },
+        sharp = "o'clock";
 
-    /** if we got a selector */
-    if( 'string' == typeof element ){
-        /** try the classic method */
-        el = document.getElementById( element.replace( /^#/, '' ) ) ||
-            /** if the browser supports advanced selectors, try finding the element this way  */
-            ( document.querySelector && ( el = document.querySelector( element ) ) );
-    } else {
-        /** assume we received an HTMLElement */
-        el = element;
-    }
-    /** we'll be using options.***, so it would suck if it was null */
-    options = options || {};
-    /** call the initialization function
-     * @see fz.init
-     */
-    return fz.init( el, options );
-};
+    translateHours[translateHours.length] = translateHours[0];
 
-if( 'undefined' != typeof jQuery )
-    jQuery.fn.fuzzyclock = function( options ){
-        return this.each( function(){
-            $( this ).data( 'fuzzyclock', new fuzzyclock( this, options ) );
-        } );
+    var fuzzy = function (date) {
+        date = date || new Date;
+        var minutes = date.getMinutes(), hours = date.getHours();
+        var data = {
+            minutes: this.understandMinutes(minutes),
+            hours: this.understandHours(hours, minutes),
+            infix: this.decideInfix(minutes),
+            suffix: this.decideSuffix(minutes),
+        };
+        var pattern = this.decidePattern(data);
+        this.data = data;
+        this.string = this.interpolate(pattern, data);
+    };
+
+    fuzzy.prototype.understandMinutes = function (minutes) {
+        if( minutes > 30 ) {
+            minutes = 60 - minutes;
+        }
+        var i = 0, key, keys = [30, 20, 15, 10, 5, 0], l = keys.length;
+        for(; i < l && minutes < keys[i]; i++);
+        return translateMinutes[keys[i]];
+    };
+
+    fuzzy.prototype.understandHours = function (hours, minutes) {
+        return translateHours[ (hours + (minutes > 30 ? 1 : 0) ) % 12 ];
+    };
+
+    fuzzy.prototype.decideInfix = function (minutes) {
+        return infix[ minutes > 30 ? 'to' : 'past' ];
+    };
+
+    fuzzy.prototype.decideSuffix = function (minutes) {
+        return minutes > 55 || minutes < 4 ? sharp : '';
+    };
+
+    fuzzy.prototype.decidePattern = function (data) {
+        return data.minutes? '{minutes} {infix} {hours}' : '{hours} {suffix}';
+    };
+
+    fuzzy.prototype.interpolate = function (pattern, data) {
+        for( var i in data ){
+            pattern = pattern.replace('{' + i + '}', data[i]);
+        }
+        return pattern;
+    };
+
+
+    var canvas = function ( element, options ) {
+        options = options || {};
+        ('interval' in options) || (options.interval = 100);
+        this.options = options;
+        if( element ){
+            this.attachTo( element );
+        }
+    };
+
+    canvas.prototype.understandElement = function (element) {
+        if( "string" === typeof element ){
+            if( 'querySelector' in document ){
+                element = document.querySelector( element );
+            } else {
+                element = document.getElementById(element.replace(/^#/, ''));
+            }
+        }
+        return element;
+    };
+
+    canvas.prototype.attachTo = function (element) {
+        this.element = this.understandElement( element );
+        if( this.element ){
+            this.startDrawing();
+        } else {
+            this.stopDrawing();
+        }
+    };
+
+    canvas.prototype.startDrawing = function () {
+        if( this.interval ) this.stopDrawing();
+        this.interval = setInterval( function (self) {
+            self.draw();
+        }, this.options.interval, this );
+        return this.draw();
     }
+
+   canvas.prototype.draw = function () {
+       this.element.innerHTML = (new fuzzy).string;
+       return this;
+   };
+
+   canvas.prototype.stopDrawing = function () {
+       this.interval && clearInterval(this.interval);
+       return this;
+   };
+
+    window.fuzzyclock = canvas;
+    window.fuzzyclock.fuzzy = fuzzy;
+
+}(window, document));
